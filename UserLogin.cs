86593925831a1130dc4a4fab0c075b1bcf1d0c3e
Alfian Yuska <60279472+Alfianyus@users.noreply.github.com>
2021-09-29 00:03:22 +0700

    userLogin
    test

---
+++ UserLogin.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginandRegister.Models
+{
+    public class UserLogin
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
