d87f7b9db97eda6ef36fa01018e3d42901f7319a
Alfian Yuska <60279472+Alfianyus@users.noreply.github.com>
2021-09-29 19:58:14 +0700

    register1.cs
    Menambahkan button register di sampil button login

---
+++ Register1.cs
@@ -0,0 +1,4 @@
+ <div class="form-group">
+                            <input type="submit" name="submit" class="btn btn-primary" value="Login" />
+                            <a href="@Url.Action("Register","Home")" class="btn btn-warning">Register</a>
+                        </div>
